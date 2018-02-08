1c117b93fefbe2bedc67b7f7666613fa09eeea28
Mahfuzur-Rahman <7mahfuzur.rahman@gmail.com>
2018-02-09 01:55:00 +0600

    Library Management Web Application Start

---
+++ Library Management Web/Library Management Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Library_Management_Web.Startup))]
+namespace Library_Management_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
